919737df270901e894db9ab724a06a77b599514b
Islan Rodrigues <jose-islan52@hotmail.com>
2018-09-09 17:09:40 -0300

    Comunicação dos botões de movimento na Main Activity

--- app/src/main/java/br/com/topicosintegradores/meujoystick/MainActivity.java
+++ app/src/main/java/br/com/topicosintegradores/meujoystick/MainActivity.java
@@ -2,12 +2,26 @@ package br.com.topicosintegradores.meujoystick;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton buttonTop, buttonBottom, buttonLeft, buttonRight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewsById();
+
     }
+
+    private void findViewsById () {
+        buttonTop = (ImageButton) findViewById(R.id.btn_top);
+        buttonBottom = (ImageButton) findViewById(R.id.btn_bottom);
+        buttonLeft = (ImageButton) findViewById(R.id.btn_left);
+        buttonRight = (ImageButton) findViewById(R.id.btn_right);
+    }
+
 }
