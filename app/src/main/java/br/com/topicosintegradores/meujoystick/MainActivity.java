ffb5a191990c96c96bc1382b7074b8e6034bdcf6
Islan Rodrigues <jose-islan52@hotmail.com>
2018-09-11 19:41:56 -0300

    Ocultando o StatusBar da tela - Tela FullScreen

--- app/src/main/java/br/com/topicosintegradores/meujoystick/MainActivity.java
+++ app/src/main/java/br/com/topicosintegradores/meujoystick/MainActivity.java
@@ -2,6 +2,8 @@ package br.com.topicosintegradores.meujoystick;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        // Ocultando a status bar da tela
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         setContentView(R.layout.activity_main);
 
         findViewsById();
